c95e53babad93c083d663ba4a23beb4a47c46c33
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-08-02 11:13:00 -0500

    basic form setup

--- int/src/app/app.component.ts
+++ int/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { ToDoListComponent } from "./to-do-list/to-do-list.component";
+import { BasicformComponent } from './basicform/basicform.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalculatorComponent, ToDoListComponent],
+  imports: [RouterOutlet, CalculatorComponent, ToDoListComponent, BasicformComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ int/src/app/basicform/basicform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basicform',
+  standalone: true,
+  imports: [],
+  templateUrl: './basicform.component.html',
+  styleUrl: './basicform.component.css'
+})
+export class BasicformComponent {
+
+}
