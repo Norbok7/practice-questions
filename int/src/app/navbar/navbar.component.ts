232a8cef7404306f908bf257fb5f3efff46a7a2b
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-08-21 12:43:20 -0500

    update to not user popper, functional dropdown, and needed updated routing to options .

--- int/src/app/navbar/navbar.component.ts
+++ int/src/app/navbar/navbar.component.ts
@@ -4,10 +4,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
+  toggleDropdown(event: Event) {
+    const target = event.target as HTMLElement;
+    const dropdownMenu = target.nextElementSibling as HTMLElement;
+    if (dropdownMenu && dropdownMenu.classList.contains('dropdown-menu')) {
+      dropdownMenu.classList.toggle('show');
+    }
+  }
 }
