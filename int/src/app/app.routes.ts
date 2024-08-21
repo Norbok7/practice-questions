35ce0217a2e8042d7e98df6cab2ad415a2d6f287
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-08-21 12:48:19 -0500

    remove test components, and routes.

--- int/src/app/app.routes.ts
+++ int/src/app/app.routes.ts
@@ -5,17 +5,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
-import { Option1Component } from './option1/option1.component';
-import { Option2Component } from './option2/option2.component';
-import { Option3Component } from './option3/option3.component';
+
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'option1', component: Option1Component },
-  { path: 'option2', component: Option2Component },
-  { path: 'option3', component: Option3Component },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- int/src/app/navbar/navbar.component.ts
+++ int/src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
@@ -13,7 +14,7 @@ export class NavbarComponent {
   toggleDropdown(event: Event) {
     const target = event.target as HTMLElement;
     const dropdownMenu = target.nextElementSibling as HTMLElement;
-    if (dropdownMenu && dropdownMenu.classList.contains('dropdown-menu')) {
+    if (dropdownMenu) {
       dropdownMenu.classList.toggle('show');
     }
   }
