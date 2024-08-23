7fe7e0afce2efb739535bc3c69796e6dd7f12e06
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-08-23 11:20:35 -0500

    routing update and css update to navbar

--- int/src/app/app.routes.ts
+++ int/src/app/app.routes.ts
@@ -1,19 +1,19 @@
-import { CalculatorComponent } from './calculator/calculator.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { HtmlComponent } from './html/html.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'services', component: ServicesComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'to-do-list', component: ToDoListComponent },
+  { path: 'html', component: HtmlComponent },
+  { path: 'javascript', component: JavascriptComponent },
   { path: 'contact', component: ContactComponent },
 ];
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
--- int/src/app/navbar/navbar.component.ts
+++ int/src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
@@ -11,11 +11,38 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class NavbarComponent {
 
+  private dropdownOpen = false;
+
+  constructor(private router: Router) {}
+
   toggleDropdown(event: Event) {
+    event.preventDefault();
     const target = event.target as HTMLElement;
     const dropdownMenu = target.nextElementSibling as HTMLElement;
     if (dropdownMenu) {
-      dropdownMenu.classList.toggle('show');
+      this.dropdownOpen = !this.dropdownOpen;
+      dropdownMenu.classList.toggle('show', this.dropdownOpen);
     }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (this.dropdownOpen && !target.closest('.dropdown-menu') && !target.closest('.dropdown-toggle')) {
+      this.dropdownOpen = false;
+      const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+      dropdownMenus.forEach(menu => menu.classList.remove('show'));
+    }
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      // Close dropdown on route change
+      if (this.dropdownOpen) {
+        this.dropdownOpen = false;
+        const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+        dropdownMenus.forEach(menu => menu.classList.remove('show'));
+      }
+    });
   }
 }
